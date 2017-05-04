e617a2a621394bb65674bdc4bea1ffa8aa730dda
David Gassner <dgassnerdev@gmail.com>
2017-05-04 14:54:23 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WeeklyTips
+This repository contains starting and ending branches for the 
+course <b>Android Development Tips Weekly</b>, available at LinkedIn Learning and Lynda.com.
---
+++ app/src/main/java/com/example/android/weeklytips/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.android.weeklytips;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private TextView mLog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mLog = (TextView) findViewById(R.id.log);
+        mLog.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    /**
+     * Run some code
+     */
+    public void runCode(View view) {
+        log("Running code");
+    }
+
+    /**
+     * Clear the output TextView
+     * @param view The button the user clicked
+     */
+    public void clearLog(View view) {
+        mLog.setText("");
+    }
+
+    /**
+     * Logs a message - called initially by runCode()
+     *
+     * @param message The message to display
+     */
+    private void log(String message) {
+        Log.i(TAG, message);
+        mLog.append(message + "\n");
+        adjustScroll();
+    }
+
+    /**
+     * Adjusts scroll vertically to ensure last line is displayed
+     */
+    private void adjustScroll() {
+        final int scrollAmount = mLog.getLayout()
+                .getLineTop(mLog.getLineCount()) - mLog.getHeight();
+        if (scrollAmount > 0)
+            mLog.scrollTo(0, scrollAmount);
+        else
+            mLog.scrollTo(0, 0);
+    }
+
+}
