5aeda475f730df286520cecc48b365776ecc9282
David Gassner <dgassnerdev@gmail.com>
2017-05-15 10:54:06 -0700

    Tip009-Done

--- app/src/main/java/com/example/android/weeklytips/MyIntentService.java
+++ app/src/main/java/com/example/android/weeklytips/MyIntentService.java
@@ -16,12 +16,7 @@ public class MyIntentService extends IntentService {
 
     public static final String TAG = "MyIntentService";
 
-    // TODO: Rename actions, choose action names that describe tasks that this
-    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
     private static final String ACTION_FOO = "com.example.android.weeklytips.action.FOO";
-    private static final String ACTION_BAZ = "com.example.android.weeklytips.action.BAZ";
-
-    // TODO: Rename parameters
     private static final String EXTRA_NAME = "com.example.android.weeklytips.extra.NAME";
     private static final String EXTRA_JOB = "com.example.android.weeklytips.extra.JOB";
 
@@ -35,27 +30,11 @@ public class MyIntentService extends IntentService {
      *
      * @see IntentService
      */
-    // TODO: Customize helper method
     public static void startActionFoo(Context context, String name, String job) {
         Intent intent = new Intent(context, MyIntentService.class);
         intent.setAction(ACTION_FOO);
         intent.putExtra(EXTRA_NAME, name);
         intent.putExtra(EXTRA_JOB, job);
-        context.startService(intent);
-    }
-
-    /**
-     * Starts this service to perform action Baz with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, MyIntentService.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_NAME, param1);
-        intent.putExtra(EXTRA_JOB, param2);
         context.startService(intent);
     }
 
@@ -67,10 +46,6 @@ public class MyIntentService extends IntentService {
                 final String name = intent.getStringExtra(EXTRA_NAME);
                 final String job = intent.getStringExtra(EXTRA_JOB);
                 handleActionFoo(name, job);
-            } else if (ACTION_BAZ.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_NAME);
-                final String param2 = intent.getStringExtra(EXTRA_JOB);
-                handleActionBaz(param1, param2);
             }
         }
     }
@@ -87,14 +62,5 @@ public class MyIntentService extends IntentService {
             e.printStackTrace();
         }
         Log.i(TAG, "handleActionFoo: name=" + name + ", job=" + job);
-    }
-
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
     }
 }
