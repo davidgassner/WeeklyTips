d7c82b258872b7881fed5de27e932430d9462887
David Gassner <dgassnerdev@gmail.com>
2017-05-15 10:57:52 -0700

    Tip010

--- app/src/main/java/com/example/android/weeklytips/MyIntentService.java
+++ app/src/main/java/com/example/android/weeklytips/MyIntentService.java
@@ -8,9 +8,6 @@ import android.util.Log;
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
- * <p>
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
  */
 public class MyIntentService extends IntentService {
 
