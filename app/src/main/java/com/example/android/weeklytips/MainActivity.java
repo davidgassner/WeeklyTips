c939abd24efe3bb4b45c27a530d77b8c098541af
David Gassner <dgassnerdev@gmail.com>
2017-08-03 12:18:07 -0700

    Tip #18

--- app/src/main/java/com/example/android/weeklytips/MainActivity.java
+++ app/src/main/java/com/example/android/weeklytips/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
         mLog.setMovementMethod(new ScrollingMovementMethod());
 
         fileHelper = new FileHelper(this);
-
     }
 
     public void playAudio(String fileName) {
