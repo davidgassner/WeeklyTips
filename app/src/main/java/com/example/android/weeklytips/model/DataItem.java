73537a1dc5acfa1cacb51e2abb0b905aed64c155
David Gassner <dgassnerdev@gmail.com>
2017-08-08 14:51:24 -0700

    Tip #22

---
+++ app/src/main/java/com/example/android/weeklytips/model/DataItem.java
@@ -0,0 +1,67 @@
+package com.example.android.weeklytips.model;
+
+import java.text.NumberFormat;
+
+public class DataItem {
+
+    private String itemName;
+    private String category;
+    private String description;
+    private int sort;
+    private double price;
+    private String image;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getSort() {
+        return sort;
+    }
+
+    public void setSort(int sort) {
+        this.sort = sort;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        return itemName + " (" + formatter.format(price) + ")";
+    }
+}
